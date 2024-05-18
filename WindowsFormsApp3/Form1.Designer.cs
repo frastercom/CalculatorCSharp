961d36af0615542b9367a20cb1396622b2580fce
admin <frastercom@gmail.com>
2024-05-18 16:46:13 +0300

    Исправление ошибок

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -50,15 +50,17 @@
             this.button21 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(3, 141);
+            this.button1.Location = new System.Drawing.Point(4, 174);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(58, 40);
+            this.button1.Size = new System.Drawing.Size(77, 49);
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,9 +69,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(67, 141);
+            this.button2.Location = new System.Drawing.Point(89, 174);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(58, 40);
+            this.button2.Size = new System.Drawing.Size(77, 49);
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
@@ -78,9 +81,10 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(131, 141);
+            this.button3.Location = new System.Drawing.Point(175, 174);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(58, 40);
+            this.button3.Size = new System.Drawing.Size(77, 49);
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
@@ -89,9 +93,10 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(3, 95);
+            this.button4.Location = new System.Drawing.Point(4, 117);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(58, 40);
+            this.button4.Size = new System.Drawing.Size(77, 49);
             this.button4.TabIndex = 3;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
@@ -100,9 +105,10 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.Location = new System.Drawing.Point(67, 95);
+            this.button5.Location = new System.Drawing.Point(89, 117);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(58, 40);
+            this.button5.Size = new System.Drawing.Size(77, 49);
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
@@ -111,9 +117,10 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button6.Location = new System.Drawing.Point(131, 95);
+            this.button6.Location = new System.Drawing.Point(175, 117);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(58, 40);
+            this.button6.Size = new System.Drawing.Size(77, 49);
             this.button6.TabIndex = 5;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
@@ -122,9 +129,10 @@
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button7.Location = new System.Drawing.Point(3, 49);
+            this.button7.Location = new System.Drawing.Point(4, 60);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(58, 40);
+            this.button7.Size = new System.Drawing.Size(77, 49);
             this.button7.TabIndex = 6;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
@@ -133,9 +141,10 @@
             // button8
             // 
             this.button8.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button8.Location = new System.Drawing.Point(67, 49);
+            this.button8.Location = new System.Drawing.Point(89, 60);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(58, 40);
+            this.button8.Size = new System.Drawing.Size(77, 49);
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
@@ -144,9 +153,10 @@
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button9.Location = new System.Drawing.Point(131, 49);
+            this.button9.Location = new System.Drawing.Point(175, 60);
+            this.button9.Margin = new System.Windows.Forms.Padding(4);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(58, 40);
+            this.button9.Size = new System.Drawing.Size(77, 49);
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
@@ -155,9 +165,10 @@
             // button10
             // 
             this.button10.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button10.Location = new System.Drawing.Point(67, 187);
+            this.button10.Location = new System.Drawing.Point(89, 230);
+            this.button10.Margin = new System.Windows.Forms.Padding(4);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(58, 40);
+            this.button10.Size = new System.Drawing.Size(77, 49);
             this.button10.TabIndex = 9;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
@@ -166,9 +177,10 @@
             // button11
             // 
             this.button11.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button11.Location = new System.Drawing.Point(131, 3);
+            this.button11.Location = new System.Drawing.Point(175, 4);
+            this.button11.Margin = new System.Windows.Forms.Padding(4);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(58, 40);
+            this.button11.Size = new System.Drawing.Size(77, 49);
             this.button11.TabIndex = 10;
             this.button11.Text = "C";
             this.button11.UseVisualStyleBackColor = true;
@@ -177,9 +189,10 @@
             // button12
             // 
             this.button12.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button12.Location = new System.Drawing.Point(67, 3);
+            this.button12.Location = new System.Drawing.Point(89, 4);
+            this.button12.Margin = new System.Windows.Forms.Padding(4);
             this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(58, 40);
+            this.button12.Size = new System.Drawing.Size(77, 49);
             this.button12.TabIndex = 11;
             this.button12.Text = "CE";
             this.button12.UseVisualStyleBackColor = true;
@@ -190,9 +203,10 @@
             this.button13.BackColor = System.Drawing.Color.Red;
             this.button13.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button13.ForeColor = System.Drawing.Color.White;
-            this.button13.Location = new System.Drawing.Point(3, 3);
+            this.button13.Location = new System.Drawing.Point(4, 4);
+            this.button13.Margin = new System.Windows.Forms.Padding(4);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(58, 40);
+            this.button13.Size = new System.Drawing.Size(77, 49);
             this.button13.TabIndex = 12;
             this.button13.Text = "⌫";
             this.button13.UseVisualStyleBackColor = false;
@@ -201,20 +215,22 @@
             // button14
             // 
             this.button14.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button14.Location = new System.Drawing.Point(131, 187);
+            this.button14.Location = new System.Drawing.Point(175, 230);
+            this.button14.Margin = new System.Windows.Forms.Padding(4);
             this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(58, 40);
+            this.button14.Size = new System.Drawing.Size(77, 49);
             this.button14.TabIndex = 13;
-            this.button14.Text = ".";
+            this.button14.Text = ",";
             this.button14.UseVisualStyleBackColor = true;
             this.button14.Click += new System.EventHandler(this.button_Click);
             // 
             // button15
             // 
             this.button15.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button15.Location = new System.Drawing.Point(195, 95);
+            this.button15.Location = new System.Drawing.Point(260, 117);
+            this.button15.Margin = new System.Windows.Forms.Padding(4);
             this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(58, 40);
+            this.button15.Size = new System.Drawing.Size(77, 49);
             this.button15.TabIndex = 14;
             this.button15.Text = "÷";
             this.button15.UseVisualStyleBackColor = true;
@@ -223,9 +239,10 @@
             // button16
             // 
             this.button16.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button16.Location = new System.Drawing.Point(195, 49);
+            this.button16.Location = new System.Drawing.Point(260, 60);
+            this.button16.Margin = new System.Windows.Forms.Padding(4);
             this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(58, 40);
+            this.button16.Size = new System.Drawing.Size(77, 49);
             this.button16.TabIndex = 15;
             this.button16.Text = "×";
             this.button16.UseVisualStyleBackColor = true;
@@ -234,9 +251,10 @@
             // button17
             // 
             this.button17.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button17.Location = new System.Drawing.Point(195, 141);
+            this.button17.Location = new System.Drawing.Point(260, 174);
+            this.button17.Margin = new System.Windows.Forms.Padding(4);
             this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(58, 40);
+            this.button17.Size = new System.Drawing.Size(77, 49);
             this.button17.TabIndex = 16;
             this.button17.Text = "+";
             this.button17.UseVisualStyleBackColor = true;
@@ -245,9 +263,10 @@
             // button18
             // 
             this.button18.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button18.Location = new System.Drawing.Point(195, 187);
+            this.button18.Location = new System.Drawing.Point(260, 230);
+            this.button18.Margin = new System.Windows.Forms.Padding(4);
             this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(57, 40);
+            this.button18.Size = new System.Drawing.Size(76, 49);
             this.button18.TabIndex = 17;
             this.button18.Text = "-";
             this.button18.UseVisualStyleBackColor = true;
@@ -256,9 +275,10 @@
             // button20
             // 
             this.button20.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button20.Location = new System.Drawing.Point(195, 3);
+            this.button20.Location = new System.Drawing.Point(260, 4);
+            this.button20.Margin = new System.Windows.Forms.Padding(4);
             this.button20.Name = "button20";
-            this.button20.Size = new System.Drawing.Size(58, 40);
+            this.button20.Size = new System.Drawing.Size(77, 49);
             this.button20.TabIndex = 19;
             this.button20.Text = "=";
             this.button20.UseVisualStyleBackColor = true;
@@ -267,9 +287,10 @@
             // button21
             // 
             this.button21.Font = new System.Drawing.Font("Bahnschrift", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button21.Location = new System.Drawing.Point(3, 187);
+            this.button21.Location = new System.Drawing.Point(4, 230);
+            this.button21.Margin = new System.Windows.Forms.Padding(4);
             this.button21.Name = "button21";
-            this.button21.Size = new System.Drawing.Size(58, 40);
+            this.button21.Size = new System.Drawing.Size(77, 49);
             this.button21.TabIndex = 20;
             this.button21.Text = "±";
             this.button21.UseVisualStyleBackColor = true;
@@ -297,26 +318,30 @@
             this.panel1.Controls.Add(this.button8);
             this.panel1.Controls.Add(this.button9);
             this.panel1.Controls.Add(this.button4);
-            this.panel1.Location = new System.Drawing.Point(12, 95);
+            this.panel1.Location = new System.Drawing.Point(16, 117);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(256, 234);
+            this.panel1.Size = new System.Drawing.Size(341, 288);
             this.panel1.TabIndex = 21;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(15, 12);
+            this.textBox1.Location = new System.Drawing.Point(20, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(249, 40);
+            this.textBox1.Size = new System.Drawing.Size(331, 48);
             this.textBox1.TabIndex = 22;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(279, 341);
+            this.ClientSize = new System.Drawing.Size(372, 420);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Tag = "";
             this.Text = "Form1";
@@ -351,6 +376,7 @@
         private System.Windows.Forms.Button button21;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -5,13 +5,20 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
-    {
+    { 
+        private static readonly string findSpaces = @"[\s]+";
+        private static readonly string findCommas = @"(?<=\d)\,(?=\d)+";
+
+        private static readonly Regex regexSpaces = new Regex(findSpaces, RegexOptions.Compiled);
+        private static readonly Regex regexCommas = new Regex(findCommas, RegexOptions.Compiled);
+
         private String d = null;
         private String oper = null;
         private bool iscleare = false;
@@ -21,32 +28,38 @@ namespace WindowsFormsApp3
             InitializeComponent();
         }
 
-        private void button13_Click(object sender, EventArgs e)
+        private void button13_Click(object sender, EventArgs e) 
         {
             if (textBox1.TextLength > 0)
             {
                 textBox1.Text = textBox1.Text.Substring(0, textBox1.TextLength - 1);
+                
             }
-           
+            if(textBox1.TextLength == 0)
+                {
+                    textBox1.Text = "0";
+                }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = "Калькулятор";
+            textBox1.Text = "0";
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "";
+            textBox1.Text = "0";
         }
 
         private void button_Click(object sender, EventArgs e)
         {
             if (iscleare)
             {
-                textBox1.Text = "";
+                textBox1.Text = "0";
             }
-            textBox1.Text = textBox1.Text + ((Button) sender).Text;
+            textBox1.Text = textBox1.Text + ((Button)sender).Text;
+            iscleare = false;
 
         }
 
@@ -55,7 +68,7 @@ namespace WindowsFormsApp3
             if (textBox1.Text == null || textBox1.Text == "")
             {
                 return;
-                
+
             }
             oper = ((Button)sender).Text;
             d = textBox1.Text;
@@ -64,7 +77,8 @@ namespace WindowsFormsApp3
 
         private void button21_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != null && textBox1.Text != "") {
+            if (textBox1.Text != null && textBox1.Text != "")
+            {
                 textBox1.Text = String.Format("{0}", double.Parse(textBox1.Text) * -1);
             }
         }
@@ -77,9 +91,10 @@ namespace WindowsFormsApp3
 
         private void button20_Click(object sender, EventArgs e)
         {
-            if (oper == "+") { 
-                    textBox1.Text = String.Format("{0}", double.Parse(textBox1.Text) + double.Parse(d));
-                    return;           
+            if (oper == "+")
+            {
+                textBox1.Text = String.Format("{0}", double.Parse(textBox1.Text) + double.Parse(d));
+                return;
             }
             if (oper == "-")
             {
@@ -91,7 +106,7 @@ namespace WindowsFormsApp3
                 if (double.Parse(textBox1.Text) != 0)
                 {
                     textBox1.Text = String.Format("{0}", double.Parse(d) / double.Parse(textBox1.Text));
-                    
+
                     return;
                 }
                 DialogResult result = MessageBox.Show(
@@ -108,5 +123,32 @@ namespace WindowsFormsApp3
                 return;
             }
         }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Count(t => t == ',') == 1)
+            {
+                return;
+            }
+           
+            try
+            {
+                double d = double.Parse(textBox1.Text);
+                textBox1.Text = String.Format("{0}", d);
+            }
+            catch
+            {
+                if (textBox1.TextLength > 0)
+                {
+                    textBox1.Text = textBox1.Text.Substring(0, textBox1.TextLength - 1);
+
+                }
+                
+            }
+
+        }
     }
 }
+  
